a93f41906e7ef3d5f840f2677c45bc73593911ec
Andre Medina <AndreMedina@hotmail.com.au>
2024-12-02 19:21:44 +1100

    Appending remaining area

--- packages/electoralyze/electoralyze/region/redistribute/mapping.py
+++ packages/electoralyze/electoralyze/region/redistribute/mapping.py
@@ -82,16 +82,55 @@ def _create_intersection_area_mapping(
     geometry_from: st.GeoDataFrame,
     geometry_to: st.GeoDataFrame,
 ) -> pl.DataFrame:
-    """Create mapping from one region to another based on intersection area."""
+    """Create mapping from one region to another based on intersection area.
+
+    Returns
+    -------
+    pl.DataFrame:
+    E.g.
+    ```
+    shape: (10, 3)
+    ┌──────────┬──────────┬───────────────────┐
+    │ region_a ┆ region_b ┆ intersection_area │
+    │ ---      ┆ ---      ┆ ---               │
+    │ str      ┆ str      ┆ f64               │
+    ╞══════════╪══════════╪═══════════════════╡
+    │ M        ┆ A        ┆ 0.25              │
+    │ M        ┆ B        ┆ 0.75              │
+    │ N        ┆ A        ┆ 0.25              │
+    │ N        ┆ C        ┆ 0.75              │
+    │ O        ┆ A        ┆ 0.75              │
+    │ O        ┆ B        ┆ 0.25              │
+    │ P        ┆ A        ┆ 0.75              │
+    │ P        ┆ C        ┆ 0.25              │
+    │ null     ┆ C        ┆ 1.0               │
+    │ null     ┆ B        ┆ 1.0               │
+    └──────────┴──────────┴───────────────────┘
+    ```
+    """
     geometry_combined = geometry_from.rename({"geometry": "geometry_from"}).join(
         geometry_to.rename({"geometry": "geometry_to"}), how="cross"
     )
     intersection_area = geometry_combined.select(
         pl.exclude("geometry_from", "geometry_to"),
         st.geom("geometry_from").st.intersection(st.geom("geometry_to")).st.area().alias("intersection_area"),
-    ).filter(pl.col("intersection_area") != 0)
+    )
 
-    return intersection_area
+    remaining_area_for_from = _get_remaining_area(
+        region_id=list(set(geometry_from.columns) - {"geometry"})[0],
+        geometry=geometry_from,
+        intersection_area=intersection_area,
+    )
+    remaining_area_for_to = _get_remaining_area(
+        region_id=list(set(geometry_to.columns) - {"geometry"})[0],
+        geometry=geometry_to,
+        intersection_area=intersection_area,
+    )
+    intersection_area_complete = pl.concat([intersection_area, remaining_area_for_from, remaining_area_for_to]).filter(
+        pl.col("intersection_area") != 0
+    )
+
+    return intersection_area_complete
 
 
 def _get_remaining_area(
@@ -100,8 +139,26 @@ def _get_remaining_area(
     intersection_area: pl.DataFrame,
 ) -> pl.DataFrame:
     """Find remaining area which hasnt been assigned to another region."""
-    # alt_region_id
-    pass
+    alt_region_id = list(set(intersection_area.columns) - {region_id, "intersection_area"})[0]
+
+    intersected_area = intersection_area.group_by(region_id).agg(
+        pl.col("intersection_area").sum().alias("intersected_area")
+    )
+    total_area = geometry.with_columns(
+        st.geom("geometry").st.area().alias("total_are"),
+        pl.lit(None).cast(pl.String).alias(alt_region_id),
+    )
+    remaining_area = (
+        total_area.join(
+            intersected_area,
+            on=region_id,
+        )
+        .with_columns(
+            pl.col("total_are").sub(pl.col("intersected_area")).alias("intersection_area"),
+        )
+        .select(intersection_area.columns)
+    )
+    return remaining_area
 
 
 def _create_centroid_distance_mapping(
