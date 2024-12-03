e4386b0dc8084025eada83c07c2e3b41ff9676ac
Andre Medina <AndreMedina@hotmail.com.au>
2024-12-03 21:24:58 +1100

    Adding link to proj.db

--- packages/electoralyze/electoralyze/common/constants.py
+++ packages/electoralyze/electoralyze/common/constants.py
@@ -7,3 +7,8 @@ ROOT_DIR: str = os.path.join(ELECTORALYZE_DIR, "../../..")
 REGION_SIMPLIFY_TOLERANCE: float = 0.0001
 
 COORDINATE_REFERENCE_SYSTEM: int = 4326
+
+# Proj.db for pyogrio
+PROD_DB_FILE = os.path.join(ROOT_DIR, ".pixi/envs/default/lib/python3.13/site-packages/pyogrio/proj_data/")
+os.environ["PROJ_LIB"] = PROD_DB_FILE
+os.environ["GDAL_DATA"] = PROD_DB_FILE
